6aa3d7b77911052a5abbf33bd9c95bfffbf5b86e
kimkvn <kimkvn@gmail.com>
2016-11-18 20:04:58 -0500

    started to set up my groundwork for the quote gen static app, but I'm having trouble getting browsersync reload to work this time in my gulp file. Moving on for now I guess

---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+// var gulp = require('gulp');
+// var sass = require('gulp-sass');
+// var browserSync = require('browser-sync').create();
+//
+// gulp.task('browserSync', function(){
+//   browserSync.init({
+//     server:{
+//       baseDir: "app"
+//     }
+//   })
+// });
+//
+// // gulp.task('sass', function(){
+// //   return gulp.src('app/scss/styles.scss')
+// //     .pipe(sass()) //using gulp sass
+// //     .pipe(gulp.dest('dist/styles'))
+// //     .pipe(browserSync.reload({
+// //       stream: true
+// //     }))
+// // });
+//
+// gulp.task('watch', ['browserSync'], function(){
+//   //gulp.watch('app/scss/*.scss', ['sass']);
+//   gulp.watch('*.html', browserSync.reload);
+//   //gulp.watch('app/*.js', browserSync.reload);
+//   //repeat above line for other watchers!
+// });
+//
+// gulp.task('default', ['browserSync', 'sass', 'watch']);
+
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+
+gulp.task('browserSync', function(){
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+});
+
+
+gulp.task('sass', function(){
+  return gulp.src('sass/styles.scss')
+    .pipe(sass()) //using gulp sass
+    .pipe(gulp.dest('styles'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+gulp.task('watch', ['browserSync', 'sass'], function(){
+  gulp.watch('sass/*.scss', ['sass']);
+  gulp.watch('*.html', browserSync.reload);
+  gulp.watch('app/**/*.js', browserSync.reload);
+  // repeat above line for other watchers!
+});
+
+gulp.task('default', ['browserSync', 'sass', 'watch' ]);
