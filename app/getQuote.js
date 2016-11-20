d9e73f2bc0a803a0d5e72f3347010dc5ae761c84
kimkvn <kimkvn@gmail.com>
2016-11-20 16:55:13 -0500

    wired the get request to the new quote button after refactoring the ajax request into its own function

--- app/getQuote.js
+++ app/getQuote.js
@@ -1,29 +1,37 @@
 $(document).ready(function(){
 
+  var quoteAPI = 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous';
   var data;
 
   /* AJAX GET method */
-  $.ajax({
-    url: 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous',
-    type: 'GET',
-    data: {},
-    dataType: 'json',
-    success: function(data){
-      console.dir((data.source));
-    },
-    error: function(err){
-      alert(err);
-    },
-    beforeSend: function(xhr){
-      xhr.setRequestHeader('X-Mashape-Authorization', "dwT2puf9UhmshZ9HCTST3CohCRrQp1NZTnBjsnRX41vK4WGw0f")
-    }
-  }).done(function(data){
-    $('.quote').html(data.quote);
-    $('.author').html(data.author);
-    console.log(data)
-  })
+  function getQuote(){
+    $.ajax({
+      url: 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous',
+      type: 'GET',
+      data: {},
+      dataType: 'json',
+      success: function(data){
+        console.dir((data.source));
+      },
+      error: function(err){
+        alert(err);
+      },
+      beforeSend: function(xhr){
+        xhr.setRequestHeader('X-Mashape-Authorization', "dwT2puf9UhmshZ9HCTST3CohCRrQp1NZTnBjsnRX41vK4WGw0f")
+      }
+    }).done(function(data){
+      $('.quote').html(data.quote);
+      $('.author').html(data.author);
+      console.log(data)
+    })
+  };
 
-  $('body').html(JSON.stringify(data))
+  getQuote();
+
+  $('.new-quote').on('click', function(){
+    getQuote();
+  });
+
 
   /* jQuery $.get method */
   // var quoteAPI = 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=movies'
