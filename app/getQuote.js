fc2db04b87be9adabb7914864685044be1193c89
kimkvn <kimkvn@gmail.com>
2016-11-21 16:51:45 -0500

    trying to figure out how to implement a crossfade transition between new photos...proving to be difficult. Might just skip this feature

--- app/getQuote.js
+++ app/getQuote.js
@@ -78,8 +78,9 @@ $(document).ready(function(){
   $('.new-quote').on('click', function(){
     $('h1, .author, .quote-mark').removeClass('focus');
     $('h1, .author, .quote-mark').addClass('blur');
+
     getQuote();
-    new getPhoto();
+    getPhoto();
   });
 
 });
